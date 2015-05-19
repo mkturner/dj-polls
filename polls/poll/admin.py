6d1b5fbbdec55594ad3c0c6f0b5c97de990c221e
Marvin T <contact@marvinkturner.com>
2015-05-18 21:44:28 -0400

    finished part 2 of tutorial

--- polls/poll/admin.py
+++ polls/poll/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from poll.models import Question, Choice
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    """allow coices to be accessed in Question page"""
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    """changing render of admin for Question"""
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields':['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
