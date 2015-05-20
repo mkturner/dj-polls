c9603dccd8092c1c257b06bdeab2047978df0492
Marvin T <contact@marvinkturner.com>
2015-05-19 23:55:53 -0400

    finished part 3

--- polls/poll/models.py
+++ polls/poll/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 # Create your models here
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -17,6 +18,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
--- polls/poll/views.py
+++ polls/poll/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from poll.models import Question
+
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World. You're at the polls index.")
+# def index(request):
+#     return HttpResponse("Hello World. You're at the polls index.")
+
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'poll/detail.html', {'question': question})
+
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+    return HttpResponse("You're looking at the results of question %s." %
+                        question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'poll/index.html', context)
