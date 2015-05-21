3e64dcae519a717be9e1e10ee811290914607b7a
Marvin T <contact@marvinkturner.com>
2015-05-20 20:15:27 -0400

    part 5: fixed future==recent bug, added tests

--- polls/poll/tests.py
+++ polls/poll/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from poll.models import Question
+
+# Create  your tests here.
+
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        '''
+            should return false if published in the Future
+        '''
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
